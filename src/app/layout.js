1573dc01bf182afa001f172d31cf869d7a54b92c
mykhailo.trunov <mykhailo.trunov@avid.com>
2024-07-23 13:52:25 +0200

    add header

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,18 +4,13 @@ import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "AI Group Chat",
-  description: "Multiple people can chat with AI in real-time.",
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
+        <title>AI Group Chat</title>
+        <meta name="description" content='Multiple people can chat with AI in real-time.' />
         <meta name="google-site-verification" content="sFjldDMNudWEpl0w-EJ74ANMaYpB17xrk5wb10lU4Vo" />
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
       </Head>
       <body className={inter.className}>
         {children}
