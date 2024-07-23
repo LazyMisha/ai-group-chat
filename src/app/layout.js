539bcff09ea851dc3ea64ed50dbe1a232444ca0e
mykhailo.trunov <mykhailo.trunov@avid.com>
2024-07-23 14:13:38 +0200

    added verification tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,19 +1,21 @@
 import { Inter } from "next/font/google";
-import Head from "next/head";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
+export const metadata = {
+  title: "AI Group Chat",
+  description: "Multiple people can chat with AI in real-time.",
+  verification: {
+    google: "sFjldDMNudWEpl0w-EJ74ANMaYpB17xrk5wb10lU4Vo",
+  },
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <title>AI Group Chat</title>
-        <meta name="description" content='Multiple people can chat with AI in real-time.' />
-        <meta name="google-site-verification" content="sFjldDMNudWEpl0w-EJ74ANMaYpB17xrk5wb10lU4Vo" />
-      </Head>
       <body className={inter.className}>
-        {children}
+          {children}
       </body>
     </html>
   );
