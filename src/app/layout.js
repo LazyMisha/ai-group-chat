954213e84fc51f4ff27232e6fc7ed664818dab53
mykhailo.trunov <mykhailo.trunov@avid.com>
2024-07-23 13:02:38 +0200

    added meta tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import Head from "next/head";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -11,8 +12,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <meta name="google-site-verification" content="sFjldDMNudWEpl0w-EJ74ANMaYpB17xrk5wb10lU4Vo" />
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+      </Head>
       <body className={inter.className}>
-          {children}
+        {children}
       </body>
     </html>
   );
