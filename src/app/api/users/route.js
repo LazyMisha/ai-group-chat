541b3ba85f21ac3b2d9e83d386460dac27bb254b
mykhailo.trunov <mykhailo.trunov@avid.com>
2024-07-25 17:55:01 +0200

    updated router

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,7 +1,13 @@
+import { revalidatePath } from 'next/cache';
 import { getUsers } from "@/lib/mongo/users";
 
 const GET = async (req, res) => {
     const users = await getUsers();
+    const path = req?.nextUrl?.pathname;
+
+    if (path) {
+        revalidatePath(path);
+    }
 
     return Response.json({ users });
 };
