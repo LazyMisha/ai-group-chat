c2f4e2c3800f73e93c4dc9c80e3a4df933a50a0c
mykhailo.trunov <mykhailo.trunov@avid.com>
2024-07-25 16:15:06 +0200

    removed cache from api requests

--- src/lib/requests.js
+++ src/lib/requests.js
@@ -1,5 +1,8 @@
 export const get = async (url) => {
-    const response = await fetch(url);
+    const response = await fetch(
+        url,
+        { cache: 'no-store' }
+    );
 
     return response.json();
 }
@@ -7,7 +10,8 @@ export const get = async (url) => {
 export const post = async (url, body) => {
     const response = await fetch(url, {
         method: 'POST',
-        body: JSON.stringify(body)
+        body: JSON.stringify(body),
+        cache: 'no-store'
     });
 
     return response.json();
@@ -16,7 +20,8 @@ export const post = async (url, body) => {
 export const put = async (url, body) => {
     const response = await fetch(url, {
         method: 'PUT',
-        body: JSON.stringify(body)
+        body: JSON.stringify(body),
+        cache: 'no-store'
     });
 
     return response.json();
@@ -24,7 +29,8 @@ export const put = async (url, body) => {
 
 export const del = async (url) => {
     const response = await fetch(url, {
-        method: 'DELETE'
+        method: 'DELETE',
+        cache: 'no-store'
     });
 
     if (response.status !== 200) {
